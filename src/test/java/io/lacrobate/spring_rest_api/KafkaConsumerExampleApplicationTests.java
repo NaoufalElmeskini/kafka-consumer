27fcb45c3c5aaaf850e649aa9a2734173c6f36c6
tintin <tintin.vocard@gmail.com>
2025-07-20 15:56:31 +0200

    refacto : reecrire minimalist testcontainer test

--- src/test/java/io/lacrobate/spring_rest_api/KafkaConsumerExampleApplicationTests.java
+++ src/test/java/io/lacrobate/spring_rest_api/KafkaConsumerExampleApplicationTests.java
@@ -2,54 +2,23 @@ package io.lacrobate.spring_rest_api;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
-import java.time.Duration;
-
-import static java.util.concurrent.TimeUnit.SECONDS;
-import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootApplication
 @Testcontainers
-
 public class KafkaConsumerExampleApplicationTests {
+	@Container
+	static final KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.2.1"));
 
-    @Container
-    static final KafkaContainer kafka =
-            new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.2.1"));
-
-    @DynamicPropertySource
-    static void overridePropertiesInternal(DynamicPropertyRegistry registry) {
-        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
-    }
-
-    @Autowired
-    private KafkaTemplate<String, Object> kafkaTemplate;
-
-    @Test
-    @DisplayName("verify container is running")
-    public void contextLoads() {
-        assertTrue(kafka.isRunning());
-    }
-
-    @Test
-    public void testConsumeEvents() {
-        System.out.println("testConsumeEvents method execution started...");
-        Customer customer = new Customer(263, "test user", "test@gmail.com", "564782542752");
-        kafkaTemplate.send("javatechie-demo", customer);
-        System.out.println("testConsumeEvents method execution ended...");
-        await().pollInterval(Duration.ofSeconds(3)).atMost(10, SECONDS)
-                .untilAsserted(() -> {
-        });
-    }
-
+	@Test
+	@DisplayName("should verify Kafka container is Running")
+	public void contextLoads() {
+		assertTrue(kafka.isRunning());
+	}
 }
